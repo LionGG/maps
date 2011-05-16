45ddb293d59c89eaa71fd302791b6dc1efe0177b
liongg <lionggwang@gmail.com>
2011-05-16 22:45:18 +0800

    chart lazy

--- douyou/douyou.js
+++ douyou/douyou.js
@@ -1,95 +1,57 @@
-﻿// by 狮兄LionGG  2011/05
-
-var latlng = new google.maps.LatLng(36.871118, 103.950511);
-var myOptions = {
-    zoom: 4,
-    center: latlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-}
-var map = new google.maps.Map(T.g("mapDiv"), myOptions);
+﻿var map = null;
 var markerArray = [];
 var geocoder = new google.maps.Geocoder();
-var infowindow = new google.maps.InfoWindow({maxWidth:270});
+var infowindow = new google.maps.InfoWindow({ maxWidth: 270 });
+var MAP_PIN_CHART_URL = "http://chart.apis.google.com/chart?chst=d_map_pin_letter_withshadow&chld=#{0}|#{1}|#{2}";
 
-var idInput = T.dom.g("idInput");
-T.event.on(idInput, "focus", function (e) {
-    T.dom.setStyle(idInput, "border", "1px solid #14568A");
-    idInput.value = "";
-});
-T.event.on(idInput, "blur", function (e) {
-    T.dom.setStyle(idInput, "border", "1px solid #CCCCCC");
-});
-T.event.on(idInput, "keydown", function (e) {
-    var event = e || windows.event;
-    if(event.keyCode === 13){
+init();
+
+function init() {
+    var idInput = T.g("idInput");
+    //var userid = T.string.trim(idInput.value);
+    T.event.on(idInput, "focus", function (e) {
+        T.dom.setStyle(idInput, "border", "1px solid #14568A");
+        idInput.value = "";
+    });
+    T.event.on(idInput, "blur", function (e) {
         T.dom.setStyle(idInput, "border", "1px solid #CCCCCC");
-        fetchUserInfo();
-    }           
-});
+    });
+    T.event.on(idInput, "keydown", function (e) {
+        var event = e || windows.event;
+        if (event.keyCode === 13) {
+            T.dom.setStyle(idInput, "border", "1px solid #CCCCCC");
+            fetchUserInfo(T.string.trim(idInput.value));
+        }
+    });
+    T.event.on(T.dom.g('btnOK'), "click", function (e) {
+        T.dom.setStyle(idInput, "border", "1px solid #CCCCCC");
+        fetchUserInfo(T.string.trim(idInput.value));
+    });
 
-T.event.on(T.dom.g('btnOK'), "click", function (e) {
-    fetchUserInfo();
-});
+    loadMap(T.g("mapDiv"));
+}
+
+function loadMap(mapElement) {
+    var latlng = new google.maps.LatLng(36.871118, 103.950511);
+    var myOptions = {
+        zoom: 4,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    map = new google.maps.Map(mapElement, myOptions);
+}
 
 var DEFAULT_ICON = "images/dou.jpg";
-var DOUBAN_PEOPLE = "http://api.douban.com/people/";
+var DOUBAN_API_PEOPLE = "http://api.douban.com/people/";
+var DOUBAN_PEOPLE = "http://www.douban.com/people/";
+var DOUBAN_API_KEY = "0a2a857383c88c2e1e7f905a8b59a48a";
 var uid = "liongg";
 var numMax = 0;
 var numFetch = 0;
 var provinceDetails = {}; //{北京:5,黑龙江:3,...}
 var cityDetails = {};  //{江苏南京:4,黑龙江大庆:1,...}    
-var iconDetails = {};  //{}
+var iconDetails = {};  //{江苏南京:头像链接集合,黑龙江大庆:头像链接集合,...}
 
-function clearLastData() {
-    numMax = 0;
-    numFetch = 0;
-    provinceDetails = {}; 
-    cityDetails = {};
-    iconDetails = {};
-}
-
-function processLocation(user) {                        
-        if(user['db:location']){
-            //资料里有location
-            var nickname = user['db:uid']['$t']; 
-            user['title']['$t'] && (nickname = user['title']['$t']);//昵称
-            var iconUrl = DEFAULT_ICON;
-            (user['link'].length > 2 && (user['link'][2]['@rel'] === 'icon')) && (iconUrl = user['link'][2]['@href']);//头像
-
-            var location = user['db:location']['$t']; //详细地址，为了地图  
-            var ulink =  "<a href='http://www.douban.com/people/" + user['db:uid']['$t']
-					    + "' target='_blank'><img src='" + iconUrl + "' title='" + nickname + "' /></a>";         
-            if (T.array.contains(T.object.keys(cityDetails), location)) {
-                cityDetails[location]++;
-                iconDetails[location] += ulink;
-            } else {
-                cityDetails[location] = 1;
-                iconDetails[location] = ulink;
-            }
-            //若已存在，数量加1；否则新增。
-                    
-
-            var pro = "";//海外 内蒙古 黑龙江...
-            if (isHaiWai(location)) {
-                pro = '海外';
-            } else {
-                isHLJorNMG(location) ? pro = location.substring(0, 3) : pro = location.substring(0, 2);
-            }
-
-            T.array.contains(T.object.keys(provinceDetails), pro) ?  provinceDetails[pro]++ : provinceDetails[pro] = 1;  
-            //若已存在，数量加1；否则新增。
-        }                      
-}
-
-function isHaiWai(location) {
-    var pattern = /^[a-zA-Z]/;
-    return pattern.test(location);
-}
-
-function isHLJorNMG(location) {
-    var str = location.substring(0, 3);
-    return (str === '黑龙江' || str === '内蒙古');
-}
 
 function delay(interval, handlers) {
     if(handlers){
@@ -142,7 +104,6 @@ function numToRGB(num){
     return "3399AA";        
 }
 
-var MAP_PIN_CHART_URL = "http://chart.apis.google.com/chart?chst=d_map_pin_letter_withshadow&chld=#{0}|#{1}|#{2}";
 function addMarker(point, city, num) {
     var strList = [];
     num = num > 100 ? 99 : num;
@@ -177,39 +138,25 @@ function deleteOverlays() {
         }); 
         markerArray.length = 0;
     }                 
-    }
-
-function drawInfo() {
-    var str = [], sum = 0, length = 0;
-    T.object.each(provinceDetails, function (value, key) {
-        str.push(key + "<span style=\"color:#389948; font-weight:bold\">" + value + "</span>");
-        sum += value;
-        length++;
-    });
-
-    T.g('infoDiv').innerHTML = "您关注了<span style=\"color:#389948; font-weight:bold\">"
-                    + numMax + "</span>位豆友，有<span style=\"color:#389948; font-weight:bold\">"
-                    + sum + "</span>位遍布全宇宙<span style=\"color:#389948; font-weight:bold\">"
-                    + length + "</span>个地方，还有<span style=\"color:#389948; font-weight:bold\">"
-                    + (numMax - sum) + "</span>位四处飘零，求包养。<br /><br />"
-					+ str.join("，") + "。";
 }
-     
-function processFriends(friends) {
-    T.array.each(friends.entry, function (item, i) {
-        processLocation(item);
-    });
-    numFetch += friends.entry.length;
-    (numFetch == numMax) && doIt(); //最后一批数据    
-}
-
+    
 function doIt() {
-    drawInfo();
-	mapIt();  
-    drawChart();     
+    initChartAPI();
+    drawInfo(provinceDetails,numMax);
+    mapIt(cityDetails,iconDetails);
 }
 
-google.load("visualization", "1", { packages: ["corechart"] });
+function loadChartAPI() {
+    google.load("visualization", "1", { packages: ["corechart"], "callback": drawChart });
+}
+
+function initChartAPI() {
+    var script = document.createElement("script");
+    script.src = "https://www.google.com/jsapi?key=ABQIAAAAKwx30XQe0xR70HffJOoPORS3ulPUy0LdEpEQBWZjPvNhTDp9jhSAQTB8cN6NHeyA2R_aoIZUb8gTuQ&callback=loadChartAPI";
+    script.type = "text/javascript";
+    document.getElementsByTagName("head")[0].appendChild(script);
+}
+
 function drawChart() {
     var data = new google.visualization.DataTable();
     data.addColumn('string', 'Province');
@@ -228,7 +175,7 @@ function drawChart() {
     });
 
     //地区大于9的话，把人数少的合并起来
-    var MAX_PIE = 10;
+    var MAX_PIE = 8;
     if (length < MAX_PIE) {
         T.object.each(provinceDetails, function (value, key) {
             data.addRow([key,value]);
@@ -243,7 +190,7 @@ function drawChart() {
         });
         if (flag === 3 && length3) {
             data.addRow(["3*"+length3, 3 * length3]);
-        } //合并一二后，还大于10，把3的也合并了
+        } //合并一二后，还大于MAX_PIE，把3的也合并了
         if (length2) {
             data.addRow(["2*"+length2, 2 * length2]);
         }
@@ -252,24 +199,107 @@ function drawChart() {
         }
     }
     T.setStyle('chartDiv', 'display', 'block');
-    var chart = new google.visualization.PieChart(document.getElementById('chartDiv'));
+    var chart = new google.visualization.PieChart(T.g('chartDiv'));
     chart.draw(data, { width: 210, height: 200, title: '地域分布' });         
+}
+
+//从豆瓣获取数据并处理
+function fetchUserInfo(userid) {
+    clearLastData();
+    uid = userid;
+    var url = DOUBAN_API_PEOPLE + uid + "/contacts?start-index=1&max-results=2"
+        + "&alt=xd&callback=fetchFriends" + "&apikey=" + DOUBAN_API_KEY;
+    uid && T.sio.callByBrowser(url);
 }
 
 function fetchFriends(userInfo) {
     numMax = userInfo['openSearch:totalResults']['$t'];
     var i = 0, url = "";
     for (; i < Math.ceil(numMax / 50); i++) {
-        url = DOUBAN_PEOPLE + uid + "/contacts?start-index=" + (i * 50 + 1) + "&max-results=50"
-            + "&alt=xd&callback=processFriends" + "&apikey=0a2a857383c88c2e1e7f905a8b59a48a";
-        T.sio.callByBrowser(url);               
+        url = DOUBAN_API_PEOPLE + uid + "/contacts?start-index=" + (i * 50 + 1) + "&max-results=50"
+            + "&alt=xd&callback=processFriends" + "&apikey=" + DOUBAN_API_KEY;
+        T.sio.callByBrowser(url);
     }
 }
 
-function fetchUserInfo() {
-    clearLastData();
-    uid = T.string.trim(idInput.value);
-    var url = DOUBAN_PEOPLE + uid + "/contacts?start-index=1&max-results=2"
-        + "&alt=xd&callback=fetchFriends" + "&apikey=0a2a857383c88c2e1e7f905a8b59a48a";
-    uid && T.sio.callByBrowser(url);
+function processFriends(friends) {
+    T.array.each(friends.entry, function (item, i) {
+        processLocation(item);
+    });
+    numFetch += friends.entry.length;
+    (numFetch == numMax) && doIt(); //最后一批数据    
 }
+
+function processLocation(user) {
+    if (user['db:location']) {
+        //资料里有location
+        var nickname = user['db:uid']['$t'];
+        user['title']['$t'] && (nickname = user['title']['$t']); //昵称
+        var iconUrl = DEFAULT_ICON;
+        (user['link'].length > 2 && (user['link'][2]['@rel'] === 'icon')) && (iconUrl = user['link'][2]['@href']); //头像
+
+        var location = user['db:location']['$t']; //详细地址，为了地图  
+        var ulink = "<a href='" + DOUBAN_PEOPLE + user['db:uid']['$t']
+					    + "' target='_blank'><img src='" + iconUrl + "' title='" + nickname + "' /></a>";
+        if (T.array.contains(T.object.keys(cityDetails), location)) {
+            cityDetails[location]++;
+            iconDetails[location] += ulink;
+        } else {
+            cityDetails[location] = 1;
+            iconDetails[location] = ulink;
+        }
+        //若已存在，数量加1；否则新增。
+
+
+        var pro = ""; //海外 内蒙古 黑龙江...
+        if (isHaiWai(location)) {
+            pro = '海外';
+        } else {
+            isHLJorNMG(location) ? pro = location.substring(0, 3) : pro = location.substring(0, 2);
+        }
+
+        T.array.contains(T.object.keys(provinceDetails), pro) ? provinceDetails[pro]++ : provinceDetails[pro] = 1;
+        //若已存在，数量加1；否则新增。
+    }
+}
+
+function clearLastData() {
+    numMax = 0;
+    numFetch = 0;
+    provinceDetails = {};
+    cityDetails = {};
+    iconDetails = {};
+}
+
+function isHaiWai(location) {
+    var pattern = /^[a-zA-Z]/;
+    return pattern.test(location);
+}
+
+function isHLJorNMG(location) {
+    var str = location.substring(0, 3);
+    return (str === '黑龙江' || str === '内蒙古');
+}
+
+//文字统计
+function drawInfo(proData, numMax) {
+    var str = [], sum = 0, length = 0;
+    T.object.each(proData, function (value, key) {
+        str.push(key + "<span style=\"color:#389948; font-weight:bold\">" + value + "</span>");
+        sum += value;
+        length++;
+    });
+
+    T.g('infoDiv').innerHTML = "您关注了<span style=\"color:#389948; font-weight:bold\">"
+                    + numMax + "</span>位豆友，有<span style=\"color:#389948; font-weight:bold\">"
+                    + sum + "</span>位遍布全宇宙<span style=\"color:#389948; font-weight:bold\">"
+                    + length + "</span>个地方，还有<span style=\"color:#389948; font-weight:bold\">"
+                    + (numMax - sum) + "</span>位四处飘零，求包养。<br /><br />"
+					+ str.join("，") + "。";
+}
+
+//地图显示
+
+
+//饼图统计
+
